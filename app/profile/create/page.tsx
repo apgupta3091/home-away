f738f5c92cfe1c64c9fb6e29b108e59747c168d0
Anuj Gupta <anuj@cyberdive.co>
2025-04-01 17:18:58 -0400

    Added prisma + supabase

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -2,16 +2,7 @@ import React from 'react';
 import FormContainer from '@/components/form/FormContainer';
 import FormInput from '@/components/form/FormInput';
 import SubmitButton from '@/components/form/Buttons';
-
-const createProfileAction = async (prevState: unknown, formData : FormData) => {
-    'use server'
-    const firstName = formData.get("firstName") as string;
-    if (firstName !== "shakeAndBake") {
-        return { message: "there was an error..." };
-    }
-    return { message: "Profile Created" };
-
-}
+import { createProfileAction } from '@/utils/actions';
 
 
 function CreateProfilePage() {
