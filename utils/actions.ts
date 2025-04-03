04b274558c558d8f52200cfed46a6a2e4b59ebf0
Anuj Gupta <anuj@cyberdive.co>
2025-04-03 17:39:05 -0400

    added profile actions

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,6 +6,16 @@ import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+const getAuthUser = async () => {
+    const user = await currentUser();
+    if (!user) {
+        throw new Error("You must be logged in to access this route");
+    }
+    if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+    return user;
+
+}
+
 export const createProfileAction = async (prevState: unknown, formData : FormData) => {
     try {
         const user = await currentUser();
@@ -27,12 +37,44 @@ export const createProfileAction = async (prevState: unknown, formData : FormDat
             }
         });
 
-        // ✅ Perform the redirect
-        redirect("/");
-
     } catch (error) {
         return { 
             message: error instanceof Error ? error.message : "An error occurred" 
         };
     }
+    redirect("/");
+
 };
+
+export const fetchProfileImage = async () => {
+    const user = await currentUser();
+    if (!user) return null;
+
+    const profile = await db.profile.findUnique({
+        where: {
+            clerkId: user.id,
+        },
+        select: {
+            profileImage: true,
+        },
+    })
+
+    return profile?.profileImage;
+}
+
+export const fetchProfile = async () => {
+    const user = await getAuthUser();
+    const profile = await db.profile.findUnique({
+        where: {
+            clerkId: user.id
+        }
+    })
+    if (!profile) redirect("/profile/create");
+    return profile;
+}
+
+
+export const updateProfileAction = async (prevState: unknown, formData: FormData) 
+:Promise<{ message: string }> => {
+    return { message: "update profile action" };
+}
