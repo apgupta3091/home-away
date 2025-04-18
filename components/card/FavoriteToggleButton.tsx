9db3b790812a72208dbfe69d1272845908fc96be
Anuj Gupta <anuj@cyberdive.co>
2025-04-17 20:55:48 -0400

    added favaorites func

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,18 +1,12 @@
-import React from "react";
-import { FaHeart } from "react-icons/fa";
-import { Button } from "../ui/button";
-import { auth } from "@clerk/nextjs/server";
-import { CardSignInButton } from "../form/Buttons";
-
-function FavoriteToggleButton({ propertyId }: Readonly<{ propertyId: string}>) {
+import { auth } from '@clerk/nextjs/server';
+import { CardSignInButton } from '../form/Buttons';
+import { fetchFavoriteId } from '@/utils/actions';
+import FavoriteToggleForm from './FavoriteToggleForm';
+async function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
   const { userId } = auth();
+  if (!userId) return <CardSignInButton />;
+  const favoriteId = await fetchFavoriteId({ propertyId });
 
-  if (!userId) return <CardSignInButton />
-  return (
-    <Button size="icon" variant="outline" className="p-2 cursor-pointer">
-      <FaHeart />
-    </Button>
-  )
+  return <FavoriteToggleForm favoriteId={favoriteId} propertyId={propertyId} />;
 }
-
 export default FavoriteToggleButton;
--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -4,7 +4,7 @@ import { ReloadIcon } from "@radix-ui/react-icons";
 import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { SignInButton } from "@clerk/nextjs";
-import { FaRegHeart } from "react-icons/fa";
+import { FaRegHeart, FaHeart} from "react-icons/fa";
 
 type btnSize = "default" | "lg" | "sm";
 
@@ -53,6 +53,26 @@ export const CardSignInButton = () => {
         <FaRegHeart />
       </Button>
     </SignInButton>
+  );
+};
+
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type='submit'
+      size='icon'
+      variant='outline'
+      className=' p-2 cursor-pointer'
+    >
+      {pending ? (
+        <ReloadIcon className=' animate-spin' />
+      ) : isFavorite ? (
+        <FaHeart />
+      ) : (
+        <FaRegHeart />
+      )}
+    </Button>
   );
 };
 
